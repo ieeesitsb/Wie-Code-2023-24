f0577f6af03adc0d32b6318845d389d9421703bf
Deeksha S <deekshas182003@gmail.com>
2023-12-08 00:30:20 +0530

    initial navbar

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,11 +1,132 @@
-import React from 'react'
-import '../header/header.css'
+// import React from 'react'
+// import '../header/header.css'
+
+
+// function Header() {
+//    return (
+//           <div className='header-main-container'>
+//           {/* Header */} 
+//           <div/>
+//    )
+//  }
+
+// export default Header
+
+import React, { useEffect } from 'react';
+import '../header/header.css';
+
 function Header() {
+  useEffect(() => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
+
+    const mobileMenu = () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    };
+
+    const closeMenu = () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    };
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+    // Cleanup the event listeners when the component unmounts
+    return () => {
+      hamburger.removeEventListener("click", mobileMenu);
+      navLink.forEach(n => n.removeEventListener("click", closeMenu));
+    };
+  }, []); // Empty dependency array ensures that this effect runs only once when the component mounts
+
   return (
     <div className='header-main-container'>
-        Header
+      {/* Header */} 
+      <header className="header">
+        <nav className="navbar">
+            <a href="#" className="nav-logo">WebDev.</a>
+            <ul className="nav-menu">
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Registration</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">About Us</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Contact Us</a>
+                </li>
+            </ul>
+            <div className="hamburger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </nav>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
+
+// import React, { useEffect } from 'react';
+// import '../header/header.css';
+
+// function Header() {
+  
+//   useEffect(() => {
+//     const hamburger = document.querySelector(".hamburger");
+//     const navMenu = document.querySelector(".nav-menu");
+
+//     const mobileMenu = () => {
+//       hamburger.classList.toggle("active");
+//       navMenu.classList.toggle("active");
+//     };
+
+//     hamburger.addEventListener("click", mobileMenu);
+
+//     // Cleanup the event listener when the component unmounts
+//     return () => {
+//       hamburger.removeEventListener("click", mobileMenu);
+//     };
+//   }, []); // Empty dependency array ensures that this effect runs only once when the component mounts
+
+//   return (
+//     <div className='header-main-container'>
+//       {/* Header */} 
+//       <header className="header">
+//         <nav className="navbar">
+//             <a href="#" className="nav-logo">WebDev.</a>
+//             <ul className="nav-menu">
+//                 <li className="nav-item">
+//                     <a href="#" className="nav-link">Home</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a href="#" className="nav-link">Registration</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a href="#" className="nav-link">About Us</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a href="#" className="nav-link">Contact Us</a>
+//                 </li>
+//             </ul>
+//             <div className="hamburger">
+//                 <span className="bar"></span>
+//                 <span className="bar"></span>
+//                 <span className="bar"></span>
+//             </div>
+//         </nav>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default Header;
