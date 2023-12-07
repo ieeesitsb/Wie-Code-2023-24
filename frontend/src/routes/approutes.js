142c275bdd47dc404d48fbcf4c7e26af9618c08a
Likith <likith1238@gmail.com>
2023-12-08 02:58:12 +0530

    pahse 1 of about us

--- frontend/src/routes/approutes.js
+++ frontend/src/routes/approutes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes,Route,BrowserRouter} from 'react-router-dom'
 import HomePage from '../pages/homePage'
+import AboutUs  from '../pages/aboutUs'
 function Approutes() {
   return (
     <div>
         <Routes>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="/about-us" element={<AboutUs/>}/>
         </Routes>
     </div>
   )
