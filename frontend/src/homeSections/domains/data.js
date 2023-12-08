1567ed48ab0bfc98706e35d90624f65261a82861
Ananya <ananyasetty127@gmail.com>
2023-12-08 21:19:15 +0530

    fourth

--- frontend/src/homeSections/domains/data.js
+++ frontend/src/homeSections/domains/data.js
@@ -1,7 +1,7 @@
 const cardData =[
     { 
         id: '1',
-        url: '../../assets/images/onr.png', 
+        url: '../../assets/images/one.png', 
         text: 'Smart City and Urban Innovation',
         desc: 'The Smart City and Urban Innovation”domain centres on leveraging technology to address challenges and enhance the overall quality of life in urban environments. Participants are encouraged to explore innovative solutions that contribute to the development of smart cities, focusing on areas such as efficient urban planning, sustainable transportation, waste management, and the integration of technology for improved city services. The domain underscores the potential of technology to create more livable, sustainable, and inclusive urban spaces, emphasising the interconnectedness of urban innovation with societal well-being.'
     },
