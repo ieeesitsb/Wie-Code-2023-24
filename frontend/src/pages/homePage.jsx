7f2ac324f71b1c2ad9335eb51ef2ff573e7cb2c4
Anagha-N13 <anagha2003.13@gmail.com>
2023-12-09 21:35:10 +0530

    prizes section added

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../homeSections/hero/heroSection'
 import Header from '../components/header/header'
 import Domains from '../homeSections/domains/domains'
+import Prizes from '../homeSections/prizes/prizes'
 // import Footer from '../components/footer/footer'
 
 function HomePage() {
@@ -9,6 +10,7 @@ function HomePage() {
     <div>
         <Header/>
         <HeroSection/>
+        <Prizes/>
         <Domains />
         {/* <Footer/> */}
     </div>
