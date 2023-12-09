6bbe5a079a17be9edc395c2d0dbf3e0af2f8f30a
Deeksha S <deekshas182003@gmail.com>
2023-12-09 21:54:14 +0530

    responsive navbar

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -57,17 +57,17 @@ function Header() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/registration" className="nav-link">
+              <Link to="/register" className="nav-link">
                 Registration
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/aboutUs" className="nav-link">
+              <Link to="/about-us" className="nav-link">
                 About Us
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/contactUs" className="nav-link">
+              <Link to="/contactus" className="nav-link">
                 Contact Us
               </Link>
             </li>
@@ -85,70 +85,3 @@ function Header() {
 
 export default Header;
 
-// import React, { useEffect } from 'react';
-// import '../header/header.css';
-// import ieeelogo from '../../assets/images/IEEE-WHITE.png';
-// import ieeeblrLogo from '../../assets/images/ieee-blr.png';
-// import wieLogo from '../../assets/images/WIELogo-removebg-preview.png';
-
-// function Header() {
-//   useEffect(() => {
-//     const hamburger = document.querySelector(".hamburger");
-//     const navMenu = document.querySelector(".nav-menu");
-//     const navLink = document.querySelectorAll(".nav-link");
-
-//     const mobileMenu = () => {
-//       hamburger.classList.toggle("active");
-//       navMenu.classList.toggle("active");
-//     };
-
-//     const closeMenu = () => {
-//       hamburger.classList.remove("active");
-//       navMenu.classList.remove("active");
-//     };
-
-//     hamburger.addEventListener("click", mobileMenu);
-
-//     navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-//     // Cleanup the event listeners when the component unmounts
-//     return () => {
-//       hamburger.removeEventListener("click", mobileMenu);
-//       navLink.forEach(n => n.removeEventListener("click", closeMenu));
-//     };
-//   }, []); // Empty dependency array ensures that this effect runs only once when the component mounts
-
-//   return (
-//     <div className='header-main-container'>
-//       <nav className="navbar">
-//         <div className="three-logo">
-//           <div><img src={ieeelogo} alt="IEEE Logo" className="ieee-logo" /></div>
-//           <div><img src={ieeeblrLogo} alt="IEEE BLR Logo" className="ieeeblr-logo" /></div>
-//           <div><img src={wieLogo} alt="WIE Logo" className="wie-logo" /></div>
-//         </div>
-
-//         <ul className="nav-menu">
-//           <li className="nav-item">
-//             <a href="#" className="nav-link">Home</a>
-//           </li>
-//           <li className="nav-item">
-//             <a href="#" className="nav-link">Registration</a>
-//           </li>
-//           <li className="nav-item">
-//             <a href="#" className="nav-link">About Us</a>
-//           </li>
-//           <li className="nav-item">
-//             <a href="#" className="nav-link">Contact Us</a>
-//           </li>
-//         </ul>
-//         <div className="hamburger">
-//           <span className="bar"></span>
-//           <span className="bar"></span>
-//           <span className="bar"></span>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Header;
