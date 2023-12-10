a7ae6d862839616a5385e598142d80836d11ff4a
Anagha-N13 <anagha2003.13@gmail.com>
2023-12-10 20:53:55 +0530

    footer added

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../homeSections/hero/heroSection'
 import Header from '../components/header/header'
 import Domains from '../homeSections/domains/domains'
 import Prizes from '../homeSections/prizes/prizes'
-// import Footer from '../components/footer/footer'
+import Footer from '../components/footer/footer'
 
 function HomePage() {
   return (
@@ -12,7 +12,7 @@ function HomePage() {
         <HeroSection/>
         <Prizes/>
         <Domains />
-        {/* <Footer/> */}
+        <Footer/>
     </div>
   )
 }
