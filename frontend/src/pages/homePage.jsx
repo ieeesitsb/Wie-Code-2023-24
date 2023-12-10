4b2352b42761c75de21d879495d1a9c0b1d00fb5
Ananya <ananyasetty127@gmail.com>
2023-12-10 23:13:52 +0530

    pulling changes

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../homeSections/hero/heroSection'
 import Header from '../components/header/header'
 import Domains from '../homeSections/domains/domains'
 import Prizes from '../homeSections/prizes/prizes'
+import Rules from '../homeSections/rules/rules'
 // import Footer from '../components/footer/footer'
 
 function HomePage() {
@@ -12,6 +13,7 @@ function HomePage() {
         <HeroSection/>
         <Prizes/>
         <Domains />
+        <Rules />
         {/* <Footer/> */}
     </div>
   )
