28e9cd4dba881463c7059d80d78596b522117978
Ananya <ananyasetty127@gmail.com>
2023-12-10 12:34:58 +0530

    change

--- frontend/src/homeSections/domains/domains.jsx
+++ frontend/src/homeSections/domains/domains.jsx
@@ -10,6 +10,7 @@ const Domains = () => {
         <div className='dom-heading'>
         <h1>Domains</h1>
         </div>
+        <div className='domain-mini-container'>
         <div className='card-d-container'>
             {cardData.map((domain) => (
             <Cards
@@ -19,6 +20,7 @@ const Domains = () => {
             desc={domain.desc}
             />
             ))}
+        </div>
         </div>
     </div>
     );
