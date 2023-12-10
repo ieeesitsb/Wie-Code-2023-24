741921ab9c9cfd6d2600c35ccb0ebdd74e2a3bca
Muzaffarmoti <muzaffarmoti@gmail.com>
2023-12-10 11:45:02 +0530

    message

--- frontend/src/pages/registration.jsx
+++ frontend/src/pages/registration.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../css/registration.css'
+import Header from '../components/header/header';
+
 function Registration() {
   return (
     <div className='content_div1  '>
+        <Header/>
       <h1>REGISTER </h1>
       <div  >
       <form className='create '>
@@ -14,10 +17,12 @@ function Registration() {
         <label>Team Size: </label>
         <select>
           <option value="select">select</option>
+          <option value="select">2</option>
           <option value="select">3</option>
           <option value="select">4</option>
         </select>
         <text>Abstract Formate(must strictly must followed)</text>
+        
         <button className='a'>Abstract</button>
       </form>
      
